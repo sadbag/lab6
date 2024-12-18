40832839ece7d5bc5b08811c5f17b93812ba8661
timoshenko vladimir <>
2024-12-18 05:00:29 +0300

    lab5

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const greeting = "Это тестовый проект";
+  const greeting_1 = "Это тестовый проект";
+  const greeting_2 = "Это тестовый проект";
+
+  return (
+    <div className="App">
+     <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+         {greeting}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders greeting message', () => {
+  render(<App />);
+  //const linkElement = screen.getByText(/Fake App Test/i);
+  const linkElement = screen.getByText(/Это тестовый проект/i);
+  expect(linkElement).toBeInTheDocument();
+});
